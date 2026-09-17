63357b4c168dc9e21f2ac97d876e04d293dc2d9d
Adrian <adrianndp@hotmail.com>
2026-09-17 12:53:17 +0200

    Remove backend files: delete app.py, extensions.py, import_jobs.py, models, views, and seed scripts

--- backend/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .job import Job, VALID_STATUSES
-from .user import User
-
-__all__ = ['Job', 'User', 'VALID_STATUSES']
--- backend/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .jobs import bp as jobs_bp
-from .users import bp as users_bp
-
-__all__ = ['jobs_bp', 'users_bp']
